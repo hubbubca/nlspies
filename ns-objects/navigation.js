5f6383c057c84022ac116365c06519567461ead2
Patrick Shields <shields.j.patrick@gmail.ca>
2015-08-26 07:21:42 -0400

    Added request and response objects

---
+++ ns-objects/navigation.js
@@ -0,0 +1,144 @@
+/**
+ * A fake nlobjFile object.
+ */
+var nlobjRequest = function(){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getAllHeaders = function (){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getAllParameters = function (){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getBody = function (){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getFile = function (id){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getHeader = function (name){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getLineItemCount = function (group){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getLineItemValue = function (group, name, line){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getMethod = function (){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getParameter = function (name){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getParameterValues = function (name){};
+
+/**
+ *
+ */
+nlobjRequest.prototype.getURL = function (){};
+
+/**
+ * A fake nlobjFile object.
+ */
+var nlobjResponse = function(){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.addHeader = function (name, value){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.getAllHeaders = function (){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.getBody = function (){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.getCode = function (){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.getError = function (){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.getAllgetHeader = function (name){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.getHeaders = function (name){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.renderPDF = function (xmlString){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.setCDNCacheable = function (type){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.setContentType = function (type, name, disposition){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.setEncoding = function (encodingType){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.setHeader = function (name, value){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.sendRedirect = function (type, identifier, id, editmode, parameters){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.write = function (output){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.writeLine = function (output){};
+
+/**
+ *
+ */
+nlobjResponse.prototype.writePage = function (pageobject){};
