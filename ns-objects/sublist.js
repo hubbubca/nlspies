53ca756c9ad112d7a58532170a746e6fa9af2f02
Patrick Shields <shields.j.patrick@gmail.ca>
2015-08-26 07:02:17 -0400

    Added sublist and renamed records to record.

---
+++ ns-objects/sublist.js
@@ -0,0 +1,84 @@
+/**
+ * A fake nlobjSubList object.
+ */
+var nlobjSubList = function (id) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.addButton = function (name, label, script) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.addMarkAllButtons = function () {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.addField = function (name, type, label, source) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.addRefreshButton = function () {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.addMarkAllButtons = function () {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.getLineItemValue = function (group, fldnam, linenum) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setAmountField = function (field) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setDisplayType = function (type) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setHelpText = function (help) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setLabel = function (label) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setLineItemValue = function (name, linenum, value) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setLineItemValues = function (values) {
+};
+
+/**
+ *
+ */
+nlobjSubList.prototype.setUniqueField = function (name) {
+};
+
