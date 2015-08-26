0d264958369f8bb9495f36c47541103f35589b22
Patrick Shields <shields.j.patrick@gmail.ca>
2015-08-26 07:36:37 -0400

    Added nlobjError

---
+++ ns-objects/error.js
@@ -0,0 +1,35 @@
+/**
+ * A fake nlobjFile object.
+ */
+var nlobjError = function(id){};
+
+/**
+ *
+ */
+nlobjError.prototype.getCode = function (){};
+
+/**
+ *
+ */
+nlobjError.prototype.getDetails = function (){};
+
+/**
+ *
+ */
+nlobjError.prototype.getId = function (){};
+
+/**
+ *
+ */
+nlobjError.prototype.getInternalId = function (){};
+
+/**
+ *
+ */
+nlobjError.prototype.getStackTrace = function (){};
+
+/**
+ *
+ */
+nlobjError.prototype.getUserEvent = function (){};
+
